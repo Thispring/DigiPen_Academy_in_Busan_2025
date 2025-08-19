7c13da460cd7c91e93274c64acd240dde3831730
Thispring <oh42gs@gmail.com>
2025-08-19 15:42:33 +0900

    update_0819

--- code/Week6/Tic-Tac-Toe_giseokOh.c
+++ code/Week6/Tic-Tac-Toe_giseokOh.c
@@ -25,6 +25,7 @@ int PlayerInput(int player); // int 매개변수로 플레이어 구별
 int CheckGameResult(); // int를 return 하여, 승패 판별
 
 int player = 1;       // 1, 2 값을 통해 플레이어 구분
+int drawCount = 0;  // 무승부 판별 변수
 char TicTacToe[3][3]; // 틱택토 배열 선언
 
 int main(void)
@@ -34,18 +35,24 @@ int main(void)
 
     do
     {
+        if (drawCount >= 9)
+        {
+            printf("draw\n");
+            break;
+        }
         player = PlayerInput(player);
         if (player == 0)
             exit(0);
         result = CheckGameResult();
-    } while (result != 0);
+    } while (result == 0);
     // 승패가 판정되지 않으면 result를 0으로 설정
+    // 승리한 player의 숫자를 반환
 
     return 0;
 }
 
 int PlayerInput(int player)
-{
+{   
     // 좌표 입력 변수 선언
     int coordX;
     int coordY;
@@ -85,18 +92,12 @@ int PlayerInput(int player)
 
 int CheckGameResult()
 {
+    // 틱택토 화면 출력
     for (int i = 0; i < 3; i++)
     {
         for (int j = 0; j < 3; j++)
         {
-            // 가로 기준 판별
-            if (TicTacToe[i][j] == 'X')
-            {
-            }
-            else if (TicTacToe[i][j] == 'O')
-            {
-            }
-            else
+            if (TicTacToe[i][j] != 'X' && TicTacToe[i][j] != 'O')
             {
                 // 입력 없는 자리에 * 임시 추가
                 TicTacToe[i][j] = '*';
@@ -106,18 +107,60 @@ int CheckGameResult()
             printf("%c", TicTacToe[i][j]);
         }
         printf("\n");
-
-        // 대각선 승리 조건
-        if (TicTacToe[0][0] == 'X' && TicTacToe[1][1] == 'X' && TicTacToe[2][2] == 'X')
-        {
-            printf("player1 Win\n");
-        }
-        else if (TicTacToe[0][0] == 'O' && TicTacToe[1][1] == 'O' && TicTacToe[2][2] == 'O')
-        {
-            printf("player2 Win\n");
-        }
-
     }
 
-    return 1;
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            // 대각선 승리 조건
+            if (TicTacToe[0][0] == 'X' && TicTacToe[1][1] == 'X' && TicTacToe[2][2] == 'X')
+            {
+                printf("player1 Win\n");
+                return 1;
+            }
+            else if (TicTacToe[0][0] == 'O' && TicTacToe[1][1] == 'O' && TicTacToe[2][2] == 'O')
+            {
+                printf("player2 Win\n");
+                return 2;
+            }
+            if (TicTacToe[0][2] == 'X' && TicTacToe[1][1] == 'X' && TicTacToe[2][0] == 'X')
+            {
+                printf("player1 Win\n");
+                return 1;
+            }
+            else if (TicTacToe[0][2] == 'O' && TicTacToe[1][1] == 'O' && TicTacToe[2][0] == 'O')
+            {
+                printf("player2 Win\n");
+                return 2;
+            }
+
+            // 가로 승리 조건
+            if (TicTacToe[j][0] == 'X' && TicTacToe[j][1] == 'X' && TicTacToe[j][2] == 'X')
+            {
+                printf("player1 Win\n");
+                return 1;
+            }
+            else if (TicTacToe[j][0] == 'O' && TicTacToe[j][1] == 'O' && TicTacToe[j][2] == 'O')
+            {
+                printf("player2 Win\n");
+                return 2;
+            }
+
+            // 세로 승리 조건
+            if (TicTacToe[0][j] == 'X' && TicTacToe[1][j] == 'X' && TicTacToe[2][j] == 'X')
+            {
+                printf("player1 Win\n");
+                return 1;
+            }
+            else if (TicTacToe[0][j] == 'O' && TicTacToe[1][j] == 'O' && TicTacToe[2][j] == 'O')
+            {
+                printf("player2 Win\n");
+                return 2;
+            }
+        }
+    }
+
+    drawCount++;
+    return 0;
 }
--- code/Week7/Pointer2DArray_giseokOh.c
+++ code/Week7/Pointer2DArray_giseokOh.c
@@ -4,3 +4,41 @@
 • Use only pointer arithmetic to access and print all elements in row-major order
 • Print the values in a formatted matrix style
 */
+#include <stdio.h>
+
+int main(void)
+{
+    int count = 1;
+    int arr[5][10] = {0};
+    int *pi;
+
+    pi = &arr[0][0];
+
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            arr[i][j] = count++;
+        }
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            printf("%d", arr[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            printf("%d", *(*(arr + i) + j));
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
